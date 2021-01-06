1d693765048ecbba1dd81b805e7a3096f614c279
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2021-01-06 18:37:16 +0200

    fixes the themes' problems

--- src/index.js
+++ src/index.js
@@ -14,27 +14,26 @@ const Theme = {
 };
 
 const themeRefs = document.querySelector('#theme-switch-toggle');
-const bodyRef = document.body;
 
-themeRefs.addEventListener('change', switchTheme);
+themeRefs.addEventListener('change', changeThemeHandler);
 
-checkTheme();
+checkThemeHandler();
 
-function checkTheme() {
-  if (localStorage.theme === 'dark-theme') {
-    bodyRef.classList.add(Theme.DARK);
-    themeRefs.checked = true;
-  } else {
-    bodyRef.classList.add(Theme.LIGHT);
-  }
-}
-
-function switchTheme(event) {
-  bodyRef.classList.toggle(Theme.LIGHT);
-  bodyRef.classList.toggle(Theme.DARK);
+function changeThemeHandler(event) {
+  document.body.classList.toggle(Theme.LIGHT);
+  document.body.classList.toggle(Theme.DARK);
   if (event.target.checked) {
     localStorage.setItem('theme', Theme.DARK);
   } else {
     localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function checkThemeHandler() {
+  if (localStorage.theme === 'dark-theme') {
+    document.body.classList.add(Theme.DARK);
+    themeRefs.checked = true;
+  } else {
+    document.body.classList.add(Theme.LIGHT);
   }
 }
